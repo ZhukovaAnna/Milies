563a55bb97da6f313d9390314f3feaec2d64dd7a
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-03-26 20:52:06 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+     int ticketprise = 60000;
+     int milies = 20;
+     int bonusmilies = ticketprise / milies;
+     System.out.println(bonusmilies);
+    }
+}
